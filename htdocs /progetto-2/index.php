981c6f0103bf44d0af047e8c45424e0d17e9207a
Margherita <mangoguglia@gmail.com>
2020-09-03 10:23:35 +0200

    php-snacks-blocco-1

--- htdocs /progetto-2/index.php
+++ htdocs /progetto-2/index.php
@@ -1,81 +1,78 @@
 <?php
-$play_1 = array(1 => 'Sassari', 'Nuoro', 50, 65);
-echo $play_1[1];
-echo '  ';
-echo $play_1[2];
-echo ' | ';
-echo $play_1[3];
-echo ' - ';
-echo $play_1[4];
-
-echo '<br>';
-
-$play_2 = array(1 => 'Frosinone', 'Milano', 40, 70);
-echo $play_2[1];
-echo '  ';
-echo $play_2[2];
-echo ' | ';
-echo $play_2[3];
-echo ' - ';
-echo $play_2[4];
-
-echo '<br>';
-
-$play_3 = array(1 => 'Imperia', 'Spezia', 15, 45);
-echo $play_3[1];
-echo '  ';
-echo $play_3[2];
-echo ' | ';
-echo $play_3[3];
-echo ' - ';
-echo $play_3[4];
-
+    $match = [
+        'pippo' => [
+            'team1' => 'Olimpia Milano',
+            'team2' => 'Cantù',
+            'score1' => 55,
+            'score2' => 60
+        ],
+        'pluto' => [
+            'team1' => 'Firenze',
+            'team2' => 'Roma',
+            'score1' => 10,
+            'score2' => 70
+        ],
+        'topolino' => [
+            'team1' => 'Torino',
+            'team2' => 'Verona',
+            'score1' => 66,
+            'score2' => 12
+        ]
+    ];
+    // foreach ($match as $m) {
+    //
+    //     echo $m['team1'] . ' - ' . $m['team2']
+    //         . ' | ' . $m['score1'] . '-' . $m['score2']
+    //         . '<br>';
+    // }
+    // for ($i=0;$i<count($match);$i++) {
+    //
+    //     $m = $match[$i];
+    //
+    //     echo $m['team1'] . ' - ' . $m['team2']
+    //         . ' | ' . $m['score1'] . '-' . $m['score2']
+    //         . '<br>';
+    // }
+    $keys = array_keys($match);
+    // var_dump($keys); die();
+    for ($i=0;$i<count($keys);$i++) {
+        $key = $keys[$i];
+        $m = $match[$key];
+        echo $m['team1'] . ' - ' . $m['team2']
+            . ' | ' . $m['score1'] . '-' . $m['score2']
+            . '<br>';
+    };
 ?>
 
 <?php
-echo '<br>';
-echo '<br>.................';
-echo '<br>';
-$name = $_GET['name'];
-$mail = $_GET['mail'];
-$age = $_GET['age'];
-echo $name;
-echo '<br>';
-echo $mail;
-echo $age;
+    $name = $_GET['name'];
+    $mail = $_GET['mail'];
+    $age  = $_GET['age'];
+    echo "name: " . $name . "<br>";
+    echo "mail: " . $mail . "<br>";
+    echo "age: " . $age . "<br>";
+    echo "<br>";
+    echo "<br>";
+    $validName = strlen($name) > 3;
+    $validMail = strpos($mail, '.') !== false &&
+                 strpos($mail, '@') !== false;
+    $validAge  = is_numeric($age);
+    if ($validName && $validMail && $validAge) {
+        echo "Accesso riuscito";
+    } else {
+        echo "Accesso negato";
+    }
+  ?>
 
 
-$countName = strlen($name);
-echo $countName;
-
-if($countName > 3){
-  echo '<br>';
-  echo 'ok';
-} else {
-  echo '<br>';
-  echo "it's so short!";
-}
-
-
-
-$find_at = '@';
-$find_dot = '.';
-$result_at = strpos($mail, $find_at);
-$result_dot = strpos($mail, $find_dot);
-if ($result_at === false || $result_dot === false) {
-
-    echo '<br>';
-    echo "<h3>Ritenta e sarai più fortunato :)</h3>";
-} else {
-    echo '<br>';
-    echo "accesso riuscito";
-};
-
-echo '<br>';
-
-if (is_numeric($age)) {
-    echo  "goal";
-} else {
-    echo "Scrivi la tua età in numeri, graze :)";
-}
- ?>
+ <?php
+ echo '<br>';
+ $rnds = [];
+    while(count($rnds) < 15) {
+        $rnd = rand(1, 15);
+        if (!in_array($rnd, $rnds)) {
+            $rnds[] = $rnd;
+        }
+    }
+    var_dump($rnds);
+?>
